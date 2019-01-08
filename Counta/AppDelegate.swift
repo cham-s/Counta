ca42ca82e46a60c24159caee47f2b8f8fc50c67f
cham-s <cc.attou@gmail.com>
2019-01-08 16:34:35 +0100

    Initial Commit

---
+++ Counta/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Counta
+//
+//  Created by chams on 08/01/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Counta/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Counta
+//
+//  Created by chams on 08/01/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
