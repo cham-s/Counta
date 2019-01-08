5547a0873f4e3a12b4f4614056b8553d29f0a781
cham-s <cc.attou@gmail.com>
2019-01-08 19:06:15 +0100

    fixed bug where nothing happened at all

--- Counta/ViewController.swift
+++ Counta/ViewController.swift
@@ -10,7 +10,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var countButton: NSButton!
     @IBOutlet weak var countLabel: NSTextField!
+    var counter = 0 {
+        didSet {
+            countLabel.stringValue = "\(counter)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +29,39 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    func startNewCounter() {
+        counter = 0
+        countLabel.stringValue = "\(counter)"
+        countButton.stringValue = "Count Down"
+        
+    }
 
     @IBAction func resetPushed(_ sender: Any) {
+        startNewCounter()
     }
     
     @IBAction func countPushed(_ sender: Any) {
+        let button = sender as! NSButton
+        
+        if button.title == "Count Down" {
+            // lauch alert
+            button.title = "Default Count"
+        } else {
+            startNewCounter()
+            
+        }
+    }
+    
+    @IBAction func incrementerPushed(_ sender: Any) {
+        let button = sender as! NSButton
+        
+        if button.title == "+" {
+            counter += 1
+        } else {
+            counter -= 1
+            counter = counter < 0 ? 0 : counter
+        }
     }
     
 }
